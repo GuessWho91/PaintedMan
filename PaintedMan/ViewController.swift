7e6ea913b1209249b87652f07b1fbb114e1cc252
Akitorg <support@akitorg.ru>
2017-12-12 20:23:44 +0200

    Initial Commit

---
+++ PaintedMan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaintedMan
+//
+//  Created by ООО АКИП on 12.12.17.
+//  Copyright © 2017 GuessWho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
